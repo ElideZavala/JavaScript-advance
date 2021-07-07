1b215822164b936ec9b1c550c5bde33f77c41312
Elide <zavalavinagreelide@gmail.com>
2021-07-06 19:42:04 -0600

    modificaciones

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const bigint = 1234567890123456789012345678901234567890n;
-const bigintTwo = BigInt('1234567890123456789012345678901234567890n')
+// const bigint = 1234567890123456789012345678901234567890n;
+// const bigintTwo = BigInt('1234567890123456789012345678901234567890n')
+
+// New String()
+// let city = new String("Mexico");
+//console.log(city);
+
+// ==> Los metodos llevan parentesis y las propidades no. 
+// Metodo toUpperCase()
+
+let movie = 'el Atlas'
+console.log(movie.toUpperCase());
+
