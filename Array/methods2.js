9f342e7e884ec78c4cad7cc072757069bc7a10ac
Elide <zavalavinagreelide@gmail.com>
2021-07-27 21:31:58 -0600

    includes & concat

---
+++ Array/methods2.js
@@ -0,0 +1,12 @@
+
+// Includes method
+// TODO: .includes =>  si se encuntra la palabra dada regresa true, si no regresa false. 
+const moods1 = ['happy', 'sad', 'satisfied'];
+console.log(moods1.includes('happy'));       // => true
+console.log(moods1.includes('mal'));         // => false
+
+// concat method
+// TODO: .concat =>  combina o fusiona dos o mas array juntos. 
+const moods2 = ['frustrated', 'uncertain', 'relaxed'];
+console.log(moods1.concat(moods2));
+console.log(moods2.concat(moods1));
