38ad6a799cba64f157d616448ecb4d16df7b7975
Elide <zavalavinagreelide@gmail.com>
2021-07-28 19:11:04 -0600

    reverse

--- Array/methods2.js
+++ Array/methods2.js
@@ -1,12 +1,21 @@
 
 // Includes method
-// TODO: .includes =>  si se encuntra la palabra dada regresa true, si no regresa false. 
+// TODO: .includes() =>  si se encuntra la palabra dada regresa true, si no regresa false. 
 const moods1 = ['happy', 'sad', 'satisfied'];
-console.log(moods1.includes('happy'));       // => true
-console.log(moods1.includes('mal'));         // => false
+// console.log(moods1.includes('happy'));       // => true
+// console.log(moods1.includes('mal'));         // => false
 
 // concat method
-// TODO: .concat =>  combina o fusiona dos o mas array juntos. 
+// TODO: .concat() =>  combina o fusiona dos o mas array juntos. 
 const moods2 = ['frustrated', 'uncertain', 'relaxed'];
-console.log(moods1.concat(moods2));
-console.log(moods2.concat(moods1));
+// console.log(moods1.concat(moods2));
+// console.log(moods2.concat(moods1));
+
+// reverse method 
+// TODO: .reverse() => Invertir el array 
+console.log(moods1.reverse());
+
+// join method
+// TODO: .join() =>
+
+
