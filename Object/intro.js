0f162fcc4904a695ef0e2b4e710b46b882b126cb
Elide <zavalavinagreelide@gmail.com>
2021-08-01 04:08:37 -0600

    object2

---
+++ Object/intro.js
@@ -0,0 +1,23 @@
+// Object Literal => Object => key-value pairs
+// const address = {
+//     country: 'Mexico',
+//     city:'Chihuahua'
+// }
+
+// console.log(address)
+
+// Acceder a los datos del abjeto
+// ==> Metodo 1 
+// console.log(address.city, "Es bien bonita");
+// console.log(address.country, "Tiene Hermosos paisajes")
+
+// ==> Metodo 2
+const person = {
+    nombre: "elide",
+    edad: 28,
+    "full address": "Atenas, Grecia",
+    1991: "eli"
+}
+
+console.log(person["edad"]);
+
