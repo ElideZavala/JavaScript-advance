5fa9f9e7c441029a5738aa5327ec5fb2a3ddbe0d
Elide <zavalavinagreelide@gmail.com>
2021-08-02 02:48:39 -0600

    met

---
+++ Object/object.js
@@ -0,0 +1,12 @@
+// => Modificar un objeto
+const movies = {
+    elOso: 9.4,
+    laRana: 9.2,
+    elGanzo: 9,
+    elLeopardo
+};
+
+// Modivar una llave
+movie["elGanzo"] = 9.9
+
+console.log(movies)
