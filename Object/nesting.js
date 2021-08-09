3fa24eb5f2879219c321c85e2336d5f75cdcb410
Elide <zavalavinagreelide@gmail.com>
2021-08-08 20:30:10 -0600

    nesting

---
+++ Object/nesting.js
@@ -0,0 +1,41 @@
+//****Nesting Object within Objects
+// const user = {
+//     thunderBold: {
+//         name: "elide",
+//         job: "programador",
+//     },
+//     grado: "A"
+// };
+
+// console.log(user["thunderBold"])
+// console.log(user["thunderBold"].name)
+// console.log(user["grado"])
+// console.log(user["thunderBold"]["name"])
+
+// const user = {
+//     thunderBold: {
+//         name: "elide",
+//         job: "programador",
+//     },
+//     grado: "A",
+//     hobbies: ['Leer', 'Meditar', 'Hacer Ejercicio']
+// };
+
+// console.log(user["hobbies"][1]);
+
+//****Nesting Object within Array
+
+const user = [
+    {
+        name: 'Elide',
+        trabajo: 'Programador',
+    },
+    "compassionate",
+    ['making suits',"destroying suits" ]
+]
+
+console.log(user[0]);
+console.log(user[0].name);
+console.log(user[2]);
+console.log(user[2][1]);
+console.log(`${user[0]["name"]} trabaja como ${user[0]["trabajo"]}`);
