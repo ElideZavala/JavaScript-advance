9bf96702bdde55edf2c3316d0d90c041d56cb74e
Elide <zavalavinagreelide@gmail.com>
2021-08-28 19:37:48 -0600

    loops-arrays

---
+++ Loops/loops-arrays.js
@@ -0,0 +1,29 @@
+// loopings Through Arrays
+// const numbers = [25, 32, 43, 45, 135];
+// console.log(numbers);
+
+// for(let e = 0; e < numbers.length; e++){
+//     console.log(e, numbers[e]);
+// }
+
+// loopings Through  Objest Nested With Arrays
+const favBooks = [
+    {
+        bookName: 'Los misterios',
+        autor: 'John lift'
+    },
+    {
+        bookName: 'La teoria de la relatividad',
+        autor: 'Franst Bers'    
+    },
+    {
+        bookName: 'El Ingenio de don quijte',
+        autor: 'Cervantes'    
+    }
+];
+
+console.log(favBooks);  
+
+for (let i = 0; i < favBooks.length; i++){
+    console.log(favBooks[i])
+}
