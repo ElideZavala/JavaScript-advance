9aaec7f1b8427a3ef3637dcf50d681cd5d6c632b
Elide <zavalavinagreelide@gmail.com>
2021-08-29 18:44:58 -0600

    while-loops

---
+++ Loops/while-loops.js
@@ -0,0 +1,21 @@
+// While loops
+
+/* 
+declare a varible  --siempre que la declaracion sea verdadera se ejecutara el codigo
+while(){
+    run de code; variable++  
+}
+*/
+// let k = 10;
+// while(k <= 10 ) {
+//     console.log(k)
+//     k++
+// }
+
+// Example 2 
+const color = ['red', 'blue', 'yellow'];
+let i = 0;
+while(i < color.length) {
+    console.log(i, color[i]);
+    i++;
+}
