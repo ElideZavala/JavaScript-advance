3d886f6d0f993ce9130a0804bda9087ac2c9d533
ElideZavala <zavalavinagreelide@gmail.com>
2021-10-15 17:34:06 -0600

    funstion methots

--- function/function-expresion.js
+++ function/function-expresion.js
@@ -11,6 +11,8 @@
 // console.log(calcNums(15, 25));
 // console.log(calcNums2(15, 25));
 
+//****Funcrions in Arrays */
+
 function avgMikeScore(score, mikeScore) {
 	return (mikeScore + 5) / score;
 }
@@ -19,10 +21,25 @@ let avgJoelScore = function (score, joeyScore) {
 	return joeyScore / score;
 }
 
-const scores = [avgMikeScore, avgJoelScore];
+// const scores = [avgMikeScore, avgJoelScore];
 
-for(let i = 0; i < scores.length; i++) {
-	let result = scores[i](100, 20);
-	console.log(result)
-}
+// for(let i = 0; i < scores.length; i++) {
+// 	let result = scores[i](100, 20);
+// 	console.log(result)
+// }
 
+// for of loop
+// for (let avgScore of scores) {
+// 		let result = avgScore(100, 20);
+// 		console.log(result  )
+// } 
+
+//****Funcrions as objects Properties
+const scores = {
+	mikeScore: avgMikeScore,
+	joelScore: avgJoelScore
+};
+
+
+console.log(scores.mikeScore(100, 20));
+console.log(scores.joelScore(100, 20));
