a30e61fe7f1f98f5917cd731e45542fa445c1841
ElideZavala <zavalavinagreelide@gmail.com>
2021-10-17 18:08:03 -0600

    selccionar children y parents

---
+++ DOM-Properties/DOM-Properties.js
@@ -0,0 +1,9 @@
+// ParentElement
+// const para = document.querySelector("p");
+// console.log(para.parentElement);
+
+// Children
+// const body1 = document.body;
+// console.log(body1.children);
+// console.log(body1.children[0]);
+// console.log(body1.children[3]);
