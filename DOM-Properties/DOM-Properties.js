cdcf0cf2f3ad2da1f8b1b90e91767595f888b9dc
ElideZavala <zavalavinagreelide@gmail.com>
2021-10-17 18:22:45 -0600

    previent and next Element

--- DOM-Properties/DOM-Properties.js
+++ DOM-Properties/DOM-Properties.js
@@ -6,4 +6,10 @@
 // const body1 = document.body;
 // console.log(body1.children);
 // console.log(body1.children[0]);
-// console.log(body1.children[3]);
+// console.log(body1.children[3])
+
+//previousElementSibling nextElementSibling
+// const item = document.querySelector("ul li:nth-child(2)")
+// console.log(item);
+// console.log(item.previousElementSibling);
+// console.log(item.nextElementSibling);
