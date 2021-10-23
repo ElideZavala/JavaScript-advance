20a77f05e8d1b273c071a9e1b106a6590c8289d6
ElideZavala <zavalavinagreelide@gmail.com>
2021-10-22 19:15:52 -0600

    append and prepend

---
+++ DOM-Properties/DOM-Properties-Two.js
@@ -0,0 +1,28 @@
+/******* Method append *******/
+
+// (1)
+const newH5 = document.createElement('h5');
+const newH6 = document.createElement('h6');
+
+// (2)
+newH5.innerHTML = "Elide";
+newH6.innerHTML = "BellaDama";
+
+// (3)
+const mainHeading = document.querySelector('h1');
+mainHeading.append(newH5, newH6);
+
+/******* Method prepend *******/
+
+// (1)
+const newH7 = document.createElement('h5');
+const newH8 = document.createElement('h6');
+
+// (2)
+newH7.innerHTML = "Perros";
+newH8.innerHTML = "Gatos";
+
+// (3)
+// const mainHeading = document.querySelector('h1');
+mainHeading.prepend(newH7, newH8);
+
